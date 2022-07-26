6a6ac38b23d1e813f9485796d80d9ae5bcbdb572
joaopedrosf <joaopedrosf1903@gmail.com>
2022-07-26 09:12:44 -0300

    Initial Commit

---
+++ RTeste/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace Realtime {
+    internal class Program {
+        static void Main(string[] args) {
+            string hubUrl = "";
+            string invokeMethodName = "";
+            string onMethodName = "";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl(hubUrl)
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.StartAsync().Wait();
+            connection.InvokeAsync(invokeMethodName, 244);
+            connection.On<object>(onMethodName, param => {
+                Console.WriteLine(param);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
